043fc222a24f3f912edac118693796755e7ec38b
Laerte <laerteakira@gmail.com>
2022-12-21 22:29:17 -0300

    feat: Address Form

--- src/pages/Purchase/index.tsx
+++ src/pages/Purchase/index.tsx
@@ -1,3 +1,66 @@
+import { CurrencyDollar, MapPinLine } from 'phosphor-react'
+import {
+  AddressFormContainer,
+  PurchaseFormContainer,
+  PaymentFormContainer,
+  BaseInputText,
+  FieldsContainer,
+} from './styles'
+
 export function Purchase() {
-  return <>Purchase</>
+  return (
+    <PurchaseFormContainer>
+      <section>
+        <h2>Complete seu pedido</h2>
+
+        <AddressFormContainer>
+          <div className="headerForm">
+            <MapPinLine />
+            <div>
+              <h3>Endereço de Entrega</h3>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </div>
+
+          <FieldsContainer>
+            <BaseInputText placeholder="CEP" width={'200px'} />
+            <BaseInputText placeholder="Rua" width={'560px'} />
+            <div className="customerInput">
+              <BaseInputText placeholder="Número" width={'200px'} />
+              <div className="optionalInput">
+                <BaseInputText placeholder="Complemento" width={'368px'} />
+                <span>Opcional</span>
+              </div>
+            </div>
+            <div className="customerInput">
+              <BaseInputText placeholder="Bairro" width={'200px'} />
+              <BaseInputText placeholder="Cidade" width={'276px'} />
+              <BaseInputText placeholder="UF" width={'60px'} />
+            </div>
+          </FieldsContainer>
+        </AddressFormContainer>
+
+        <PaymentFormContainer>
+          <div className="headerForm">
+            <CurrencyDollar />
+            <div>
+              <h3>Pagamento</h3>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <input type="radio" id="credit" name="typePayment" value="credit" />
+            <label htmlFor="credit">CARTÃO DE CRÉDITO</label>
+            <input type="radio" id="debit" name="typePayment" value="debit" />
+            <label htmlFor="debit">CARTÃO DE DÉBITO</label>
+            <input type="radio" id="cash" name="typePayment" value="cash" />
+            <label htmlFor="cash">DINHEIRO</label>
+          </div>
+        </PaymentFormContainer>
+      </section>
+    </PurchaseFormContainer>
+  )
 }
--- src/pages/Purchase/styles.ts
+++ src/pages/Purchase/styles.ts
@@ -1,3 +1,88 @@
 import styled from 'styled-components'
 
-export const PurchaseContainer = styled.div``
+export const PurchaseFormContainer = styled.form`
+  padding: 2.5rem 0;
+
+  display: grid;
+  grid-template-columns: 1fr 28rem;
+  gap: 2rem;
+
+  h2 {
+    font-family: 'Baloo 2', sans-serif;
+    color: ${(props) => props.theme['base-subtitle']};
+    font-weight: 700;
+    margin-bottom: 0.9rem;
+  }
+`
+
+const BaseFormContainer = styled.div`
+  padding: 2.8rem;
+  margin-bottom: 0.75rem;
+  background-color: ${(props) => props.theme['base-card']};
+  border-radius: 6px;
+
+  .headerForm {
+    display: flex;
+    gap: 0.5rem;
+    margin-bottom: 2rem;
+  }
+
+  h3 {
+    font-weight: 400;
+    font-size: 1rem;
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+
+  p {
+    font-size: 0.875rem;
+    color: ${(props) => props.theme['base-text']};
+  }
+`
+
+export const AddressFormContainer = styled(BaseFormContainer)`
+  color: ${(props) => props.theme['yellow-dark']};
+`
+
+export const PaymentFormContainer = styled(BaseFormContainer)`
+  color: ${(props) => props.theme.purple};
+`
+
+export const FieldsContainer = styled.div`
+  .customerInput {
+    display: flex;
+    justify-content: space-between;
+    flex: 1;
+  }
+
+  .optionalInput {
+    display: flex;
+    flex: 1;
+    position: relative;
+    max-width: 20.5rem;
+  }
+
+  .optionalInput span {
+    position: absolute;
+    top: 30px;
+    right: 20px;
+
+    font-style: italic;
+    font-size: 0.875rem;
+    color: ${(props) => props.theme['base-label']};
+  }
+`
+
+export const BaseInputText = styled.input`
+  border: 1px solid ${(props) => props.theme['base-button']};
+  border-radius: 4px;
+  background-color: ${(props) => props.theme['base-input']};
+  color: ${(props) => props.theme['base-text']};
+  padding: 0.875rem;
+  max-width: ${(props) => props.width};
+  width: 100%;
+  margin-top: 1rem;
+
+  &::placeholder {
+    color: ${(props) => props.theme['base-label']};
+  }
+`
