d6e6d44824251f1227a514cc28a6c11b2a761167
Laerte <laerteakira@gmail.com>
2022-12-26 11:21:34 -0300

    feat: Navigation Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,20 +2,27 @@ import { MapPin, ShoppingCart } from 'phosphor-react'
 
 import { CartNavOption, HeaderContainer, LocationNavOption } from './styles'
 import logoTipo from '../../assets/logoTipo.svg'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <img src={logoTipo} alt="" />
+      <NavLink to="/">
+        <img src={logoTipo} alt="" />
+      </NavLink>
 
       <nav>
-        <LocationNavOption href="#">
-          <MapPin size={'18px'} />
-          São Paulo, SP
-        </LocationNavOption>
-        <CartNavOption href="#">
-          <ShoppingCart size={'18px'} />
-        </CartNavOption>
+        <NavLink to="/purchase-made">
+          <LocationNavOption href="#">
+            <MapPin size={'18px'} />
+            São Paulo, SP
+          </LocationNavOption>
+        </NavLink>
+        <NavLink to="/purchase">
+          <CartNavOption href="#">
+            <ShoppingCart size={'18px'} />
+          </CartNavOption>
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
